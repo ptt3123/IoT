b2651c97967805b3d24bd85b1730b8af16b8b6a5
Phan Tien Tai <143255769+ptt3123@users.noreply.github.com>
2024-08-19 23:27:29 +0700

    Add files via upload

---
+++ IoTProject/core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+# Create your views here.
+class Index(APIView):
+    def get(self, request):
+        return render(request, 'core/layout.html')
